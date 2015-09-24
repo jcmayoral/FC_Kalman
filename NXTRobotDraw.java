eeb2b36b70bd48625f64e8661dddae61112e9d26
jcmayoral <jocamaba1989@gmail.com>
2015-09-24 12:12:09 +0200

    Working with Filter Problems

--- NXTRobotDraw.java
+++ NXTRobotDraw.java
@@ -10,43 +10,58 @@ public class NXTRobotDraw{
      * @param args
      */
 	
+	//Side Sizes
+	public static final int side=900;
+	public static final int side2=1800;
 	
 	//Instancing Attributes
 	public static CompassHTSensor comp=new CompassHTSensor(SensorPort.S2);
     public static AccelHTSensor accel=new AccelHTSensor(SensorPort.S4);
     
-    
+    // Filter Auxiliar
+    public static double last=0.0;
+
     //Amazing Class for Integrations
     //speed to position
-    public static Integration Ipos=new Integration(0,0);
+    public static Integration Ipos;
     //acceleration to speed
-    public static Integration Ivel=new Integration(0,0);
+    public static Integration Ivel;
+
     
     
     
     
     //Filtering Degree
-	public static double getDegreeCalc (double degree){
+	public static double getDegreeCalc(){
 		
-		double tau=0.3;
-        double aux=0.0;
-        double e=0.0,last=0.0;		
+		//
+		double tau=0.5,degree;
+        
+        double aux=0.0,e=0.0;		
 		//Filter
     	aux=comp.getDegreesCartesian();
+    	e=(last-aux)*tau;
     	last=aux;
-    	e=(last-aux)*tau+e;
     	degree=e+aux;
+    	
     	return degree;
+    	
 	}
 	
 	
+	
 	//Calculating 
-	public static double CalculatePosition() {
-		
+	public static double CalculatePosition(Integration Iv,Integration Ip) {
+	
+	    
 		double result;	
-		result=(double)accel.getZAccel();
-		result=Ivel.addReading(result);
-		result=Ipos.addReading(result);
+//		result=(double)accel.getZAccel();
+		//Obtaining Acceleration Value Axis X
+		result=(double)accel.getXAccel();
+		//Integration for obtaining speed
+		result=Iv.addReading(result);
+		//Integrating again for obtaining position
+		result=Ip.addReading(result);
 		
 		return result;
 		
@@ -87,26 +102,27 @@ public class NXTRobotDraw{
  * start with with q, r, p and x
  */
         
-        //double q=0.2,r=0.1,p=1,k=1,x; Kalman
-        double posChange=0;
+        double posChange;
         double initialDegree;
         double finalDegree;
         double accumPos;
-        
+        boolean flag=true;
         
         //Starting Compass
         comp.startCalibration();
         Thread.sleep(3000);
         comp.stopCalibration();
+        
+        //Reset Cartesian Degrees
         comp.resetCartesianZero();
      
         //Setting Speed
-        Motor.A.setSpeed(20);
-        Motor.C.setSpeed(20);
+        Motor.A.setSpeed(30);
+        Motor.C.setSpeed(30);
              
         
         //while (!touch.isPressed())
-        for(int i=0;i<1;i++)
+        for(int i=0;i<12;i++)
         {
 //KALMAN
 //        	x=comp.getDegreesCartesian();
@@ -114,43 +130,93 @@ public class NXTRobotDraw{
 //        	k=p/(p+r);
 //        	degree=x+k*(degree-x);
 //        	p=(1-k)*p;
+        	
+        	
+            
+            //speed to position
+            Ipos=new Integration(0,0);
+            //acceleration to speed
+            Ivel=new Integration(0,0);
 
         	
-        	//Complementary
-        
-        	posChange=CalculatePosition();
-        	accumPos=posChange+1000;
+        	//Calculating Position from Accelerometer        
+        	posChange=CalculatePosition(Ivel,Ipos);
+        	       	
+        	//Setting final Position
+        	if(i%2==0) //long Side
+        	accumPos=posChange+side2;
+        	else //ShortSide
+        	accumPos=posChange+side;
         	
-        	do
-        	{
-        	posChange=CalculatePosition();
+        	
+        	
+        	//Displaying Values
+        	System.out.print ("Current Pos."+posChange+"\n");
+        	System.out.print("Final Pos. "+accumPos+"\n");
+        	
+        	//Stop without Inertia
+        	Motor.A.stop(true);
+        	Motor.C.stop(true);
+        	
+        	//Wait to See
+        	Thread.sleep(1000);
+        	
+        	//Rotating Robot
         	Motor.A.forward();
             Motor.C.forward();
-            	
-        	}
-        	while (posChange<accumPos);       	
         	
-        	initialDegree=comp.getDegreesCartesian();
-        	System.out.print(initialDegree+"initial \n");
-        	Thread.sleep(3000);
+            
+            //Going to front
+            while (posChange<accumPos)
+        	posChange=CalculatePosition(Ivel,Ipos);
+
+            
+            //Stop without Inertia
+        	Motor.A.stop(true);
+        	Motor.C.stop(true);
         	
+        	
+        	// Calculating Degrees
+        	initialDegree=getDegreeCalc();        	
         	finalDegree=initialDegree+90.0;
         	
+        	
+        	//If InitialDegree is more than 270°
         	if (finalDegree>360)
         		finalDegree=finalDegree-360;     	
-        	
-        	System.out.print(finalDegree+"final \n");
-        	Thread.sleep(3000);
-        	
+        	        	
+         	System.out.print(initialDegree+" I.Deg. \n");
+        	System.out.print(finalDegree+" F.Deg. \n");
+
+        	//Wait to See Screen
+        	Thread.sleep(1000);
+           	
+        	//Starts Rotating
         	Motor.A.forward();
-            Motor.C.backward();        	
+            Motor.C.backward();
+            
+           
+            
         	do
         	{               
-        	initialDegree=getDegreeCalc(initialDegree);
+        	initialDegree=getDegreeCalc();
         	System.out.print(initialDegree+"\n");
-        	}
-        	while (initialDegree<finalDegree);
 
+        	//When Movement doesn't pass by 0°
+        	if (finalDegree>=90)
+        		if (initialDegree>=finalDegree )
+        			flag=false;
+        							
+        	//When Movement pass by 0°
+        	if (finalDegree<90)
+        		if (finalDegree>=initialDegree)
+        			flag=false;
+        	}
+        	while (flag);
+
+        	 //Reset Rotating
+            flag=true;
+        	
         }
  
     
